e903d1bcfc3818ab0a1ddae09e035b3f7582565e
konglingkai <klk1129635667@outlook.com>
2016-09-20 20:56:08 +0800

    first commit

---
+++ app/src/main/java/com/example/merkk/merk/MainAcitivity.java
@@ -0,0 +1,16 @@
+package com.example.merkk.merk;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by acer on 2016/9/20.
+ */
+public class MainAcitivity  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.weather_info);
+    }
+}
